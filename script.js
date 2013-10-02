5d52ab2a41a6503ddf3b975095b85a3edca33be9
Anastasiya Sutyagina <asutyagina@gmail.com>
2013-10-02 16:15:56 -0700

    fixed webs wins on columns

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 var playerTurn = 1;
-
-function navCells() {
 var cellArray =[["","",""],["","",""],["","",""]];
+function navCells() {
 
 
 for(r=0;r<=2;++r)
@@ -20,51 +19,53 @@ for(r=0;r<=2;++r)
 		}
 	}
 
-
-
 for(x=0;x<=2;x++)
 {
 	if(cellArray[0][x] == cellArray[1][x] &&
 		cellArray[1][x] == cellArray[2][x] && 
 		cellArray[0][x] != "" &&
 		cellArray[0][x] == '<img src="tick_tock.jpg">')
+
 				document.getElementById("tick_tock").style.display="block";
-			else if (cellArray[0][x] == cellArray[1][x] &&
-					cellArray[1][x] == cellArray[2][x] && 
-					cellArray[0][x] != "" &&
-					cellArray[0][x] == '<img src ="web.jpeg">') 
-						document.getElementById("web").style.display="block";
+	if (cellArray[0][x] == cellArray[1][x] &&
+		cellArray[1][x] == cellArray[2][x] && 
+		cellArray[0][x] != "" &&
+		cellArray[0][x] == '<img src="web.jpeg">') 
+				document.getElementById("web").style.display="block";
+
 //alert(cellArray[0][x] + " won in column " + (x+1));
 	if(cellArray[x][0] == cellArray[x][1] &&
 		cellArray[x][1] == cellArray[x][2] && 
 		cellArray[x][0] != "" &&
 		cellArray[x][0] == '<img src="tick_tock.jpg">')
 				document.getElementById("tick_tock").style.display="block";
-			else if (cellArray[x][0] == cellArray[x][1] &&
-					cellArray[x][1] == cellArray[x][2] && 
-					cellArray[x][0] != "" &&
-					cellArray[x][0] == '<img src="web.jpeg">')
-							document.getElementById("web").style.display="block";
-//alert(cellArray[x][0] + " won in row " + (x+1));
-	
 
-if(cellArray[0][0] == cellArray[1][1] &&
+	if (cellArray[x][0] == cellArray[x][1] &&
+		cellArray[x][1] == cellArray[x][2] && 
+		cellArray[x][0] != "" &&
+		cellArray[x][0] == '<img src="web.jpeg">')
+				document.getElementById("web").style.display="block";
+//alert(cellArray[x][0] + " won in row " + (x+1));
+
+	if(cellArray[0][0] == cellArray[1][1] &&
 	   cellArray[1][1] == cellArray[2][2] &&
 	   cellArray[0][0] != "" &&
 	   cellArray[0][0] == '<img src="tick_tock.jpg">')
 	   			document.getElementById("tick_tock").style.display="block";
-	   		else if (cellArray[0][0] == cellArray[2][2] &&
+
+	if (cellArray[0][0] == cellArray[2][2] &&
 	   cellArray[1][1] == cellArray[2][2] &&
 	   cellArray[0][0] != "" &&
 	   cellArray[0][0] == '<img src="web.jpeg">')
 	   			document.getElementById("web").style.display="block";
-	
+
 	if(cellArray[0][2] == cellArray[1][1] &&
 	   cellArray[1][1] == cellArray[2][0] &&
 	   cellArray[0][2] != "" &&
 	   cellArray[0][2] == '<img src="tick_tock.jpg">')
 	   			document.getElementById("tick_tock").style.display="block";
-	   		else if (cellArray[0][2] == cellArray[1][1] &&
+	   		
+	if (cellArray[0][2] == cellArray[1][1] &&
 	   cellArray[1][1] == cellArray[2][0] &&
 	   cellArray[0][2] != "" &&
 	   cellArray[0][2] == '<img src="web.jpeg">')
@@ -73,4 +74,3 @@ if(cellArray[0][0] == cellArray[1][1] &&
 		}
 	}
 }
-
